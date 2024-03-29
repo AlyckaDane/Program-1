eed1600a3e5b410cae45907aa031e81bc78c4e81
AlyckaDane <165013164+AlyckaDane@users.noreply.github.com>
2024-03-29 22:24:37 +0800

    Create Program.cs

---
+++ carassco/carassco/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string username, password;
+        int attempts = 3;
+
+        while (attempts > 0)
+        {
+            Console.Write("Enter username: ");
+            username = Console.ReadLine();
+
+            Console.Write("Enter password: ");
+            password = Console.ReadLine();
+
+            if (username == "Admin" && password == "Admin1234!")
+            {
+                Console.WriteLine("Login successful");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect credentials. Please try again.");
+                attempts--;
+            }
+        }
+
+        if (attempts == 0)
+        {
+            Console.WriteLine("Too many failed attempts. Please try again later.");
+        }
+    }
+}
